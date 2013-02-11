763c6053557ae53857ca7e2cb96eca7c12385e1c
Tommy Hodgins <tomhodgins@gmail.com>
2013-02-10 23:21:39 -0500

    Popovers and Tooltips enabled!

--- js/boilerstrap.js
+++ js/boilerstrap.js
@@ -9,5 +9,18 @@ $( document ).ready(function() {
 	});
 	
 	// Gesture Shortcuts (via hammer.js)
+	
+	
+	// Initialize Bootstrap's tooltips
+	$('a[data-toggle=tooltip]').tooltip()
+	$('a[data-toggle=tooltip-left]').tooltip({placement: 'left'})
+	$('a[data-toggle=tooltip-right]').tooltip({placement: 'right'})
+	$('a[data-toggle=tooltip-down]').tooltip({placement: 'bottom'})
+		
+    // Inititialize Bootstrap's popovers
+    $("a[data-toggle=popover]").popover({html: 'true', placement: 'top'}).click(function(e) {e.preventDefault(); });    
+    $("a[data-toggle=popover-left]").popover({html: 'true', placement: 'left'}).click(function(e) {e.preventDefault(); });
+    $("a[data-toggle=popover-right]").popover({html: 'true', placement: 'right'}).click(function(e) {e.preventDefault(); });
+    $("a[data-toggle=popover-down]").popover({html: 'true', placement: 'bottom'}).click(function(e) {e.preventDefault(); });
 
 });
