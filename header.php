42c1e70a0420fc431e0310c37678b8f732369968
Tommy Hodgins <tomhodgins@gmail.com>
2013-03-07 14:29:40 -0500

    Fixed favicon link

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
     <meta property="og:image" content="" />
 
     <!-- Fav and touch icons -->
-    <link rel="shortcut icon" href="img/ico/favicon.ico">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/img/ico/favicon.ico">
     <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/img/ico/apple-touch-icon-144-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/ico/apple-touch-icon-114-precomposed.png">
     <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/ico/apple-touch-icon-72-precomposed.png">
